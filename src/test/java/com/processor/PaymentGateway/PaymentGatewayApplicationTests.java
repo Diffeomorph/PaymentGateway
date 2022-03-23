bce60d499e5be9f8b8fad3e88f4b65684ae9cc6d
Diffeomorph <64492048+Diffeomorph@users.noreply.github.com>
2022-03-23 19:50:20 +0000

    blank spring project

---
+++ src/test/java/com/processor/PaymentGateway/PaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.processor.PaymentGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
