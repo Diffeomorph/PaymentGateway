08259c4841a82664257c4b8a4416c50984611195
Diffeomorph <64492048+Diffeomorph@users.noreply.github.com>
2022-03-27 15:19:49 +0100

    testing file rewrite

--- src/test/java/com/processor/PaymentGateway/PaymentGatewayApplicationTests.java
+++ src/test/java/com/processor/PaymentGateway/PaymentGatewayApplicationTests.java
@@ -2,27 +2,21 @@ package com.processor.PaymentGateway;
 
 import com.processor.PaymentGateway.Controllers.AcquiringBankController;
 import com.processor.PaymentGateway.Services.Payment;
+import com.processor.PaymentGateway.Services.Payments;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 class PaymentGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
-	@Mock
 	Payment mockPayment;
 
 	@Test
@@ -41,13 +35,12 @@ class PaymentGatewayApplicationTests {
 	@InjectMocks
 	AcquiringBankController acquiringBankController;
 
-	@Autowired
-	public MockMvc mockMvc;
-
 	@Test
 	public void testSubmitPayment() throws Exception {
 		mockPayment = new Payment("1234567891234567", "03/20", 1000, "USD", 456, false);
-		mockMvc.perform(post("http://localhost:8080/payments",mockPayment)).andExpect(MockMvcResultMatchers.content().string("true"));
+		Payments payments = new Payments();
+		boolean result = payments.submitPayment(mockPayment);
+		assertEquals(result, true);
 	}
 
 
