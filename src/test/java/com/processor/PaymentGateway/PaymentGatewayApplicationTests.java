fc72a60d4b30c6f39e411e4a895eafc99e4ba2ee
Diffeomorph <64492048+Diffeomorph@users.noreply.github.com>
2022-03-29 20:25:24 +0100

    added correct autowiring, correct mockPayment

--- src/test/java/com/processor/PaymentGateway/PaymentGatewayApplicationTests.java
+++ src/test/java/com/processor/PaymentGateway/PaymentGatewayApplicationTests.java
@@ -1,47 +1,48 @@
 package com.processor.PaymentGateway;
 
-import com.processor.PaymentGateway.Controllers.AcquiringBankController;
 import com.processor.PaymentGateway.Services.Payment;
 import com.processor.PaymentGateway.Services.Payments;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class PaymentGatewayApplicationTests {
+
+	@Autowired
+	Payments payments;
+	Payment mockPayment;
 
 	@Test
 	void contextLoads() {
 	}
 
-	Payment mockPayment;
+	@BeforeEach
+	public void setUp() throws Exception {
+		mockPayment = new Payment("1234567891234567", "03/20", 1000, "USD", 456, false);
+	}
 
 	@Test
-	void checkValidation(){
-		mockPayment = new Payment("1234567891234567", "03/20", 1000, "USD", 456, false);
+	void testValidation(){
 		assertEquals(mockPayment.performValidation(), true);
 	}
 
 	@Test
-	void checkMasking(){
-		mockPayment = new Payment("1234567891234567", "03/20", 1000, "USD", 456, false);
+	void testMasking(){
 		mockPayment.maskCardNumber();
 		assertEquals(mockPayment.getCardNumber().substring(0,12),"############");
 	}
 
-	@InjectMocks
-	AcquiringBankController acquiringBankController;
-
 	@Test
 	public void testSubmitPayment() throws Exception {
-		mockPayment = new Payment("1234567891234567", "03/20", 1000, "USD", 456, false);
-		Payments payments = new Payments();
 		boolean result = payments.submitPayment(mockPayment);
 		assertEquals(result, true);
 	}
-
 
 }
